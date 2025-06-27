baf4fe5aafa64b8b8bb6bec61d341ae3862d0323
kribo852 <kristoffer.a.borg@gmail.com>
2025-06-27 23:19:00 +0200

    Refactored the library
    Extracted a function for finding the genome with the highest score.
    Changed a couple of type-casts from "try_into.unwrap()" to "as u32".
    Formatted the code with the rust formatter.

--- src/lib.rs
+++ src/lib.rs
@@ -2,94 +2,122 @@ use rand::Rng;
 
 struct SimulationInstance<GenomeType> {
     genome: GenomeType,
-    score: f64
+    score: f64,
 }
 
 const COMPLETE_VECTOR_MAX_SIZE: usize = 100;
 
-
-pub fn run_simulation<GenomeType: Clone>(new_genome: fn() -> GenomeType, 
-                                score_fn: fn(genome: &GenomeType) -> f64,
-                                output: fn(genome: &GenomeType),
-                                mutators: Vec<fn(genome: &GenomeType) -> GenomeType>,
-                                finish: fn(high_score: f64) -> bool) {
-
-
+pub fn run_simulation<GenomeType: Clone>(
+    new_genome: fn() -> GenomeType,
+    score_fn: fn(genome: &GenomeType) -> f64,
+    output: fn(genome: &GenomeType),
+    mutators: Vec<fn(genome: &GenomeType) -> GenomeType>,
+    finish: fn(high_score: f64) -> bool,
+) {
     let mut complete: Vec<SimulationInstance<GenomeType>> = vec![];
     let mut not_yet_complete: Vec<SimulationInstance<GenomeType>> = vec![];
 
     let mut high_score = 0.0;
 
     while !finish(high_score) {
+        let run_sim_instance =
+            get_sim_instance_to_run(new_genome, &mut complete, &mut not_yet_complete);
+        let mut genome_array: [GenomeType; 8] = [
+            run_sim_instance.genome.clone(),
+            get_mutated_genome(&run_sim_instance.genome, &mutators),
+            get_mutated_genome(&run_sim_instance.genome, &mutators),
+            get_mutated_genome(&run_sim_instance.genome, &mutators),
+            get_mutated_genome(&run_sim_instance.genome, &mutators),
+            get_mutated_genome(&run_sim_instance.genome, &mutators),
+            get_mutated_genome(&run_sim_instance.genome, &mutators),
+            get_mutated_genome(&run_sim_instance.genome, &mutators),
+        ];
+        let mut measured_scores = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
 
-        let run_sim_instance = get_sim_instance_to_run(new_genome, &mut complete, &mut not_yet_complete);
-        let mut genome_array: [GenomeType; 8] = [run_sim_instance.genome.clone(), 
-        get_mutated_genome(&run_sim_instance.genome, &mutators), 
-        get_mutated_genome(&run_sim_instance.genome, &mutators), 
-        get_mutated_genome(&run_sim_instance.genome, &mutators), 
-        get_mutated_genome(&run_sim_instance.genome, &mutators), 
-        get_mutated_genome(&run_sim_instance.genome, &mutators), 
-        get_mutated_genome(&run_sim_instance.genome, &mutators), 
-        get_mutated_genome(&run_sim_instance.genome, &mutators)];
-        let mut calc_score = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
-
-        for index in 0..calc_score.len() {
-            calc_score[index] = score_fn(&genome_array[index]);
+        for index in 0..measured_scores.len() {
+            measured_scores[index] = score_fn(&genome_array[index]);
         }
 
-        for _iterations in 1..100 {
+        for _iterations in 1..1000 {
             let mut rng = rand::rng();
 
-            let mutated_genome = get_mutated_genome(&genome_array[rng.random_range(0..calc_score.len())], &mutators);
+            let mutated_genome = get_mutated_genome(
+                &genome_array[rng.random_range(0..measured_scores.len())],
+                &mutators,
+            );
             let temp_score = score_fn(&mutated_genome);
 
-            for index in 0..calc_score.len() {
-                if temp_score > calc_score[index] {
-                    calc_score[index] = temp_score;
+            for index in 0..measured_scores.len() {
+                if temp_score > measured_scores[index] {
+                    measured_scores[index] = temp_score;
                     genome_array[index] = mutated_genome;
                     break;
                 }
-            }    
-        }
-        let mut highest_new_score = 0.0;
-        let mut best_new_genome = genome_array[0].clone();
-        for index in 0..calc_score.len() {
-            if calc_score[index] > highest_new_score {
-                highest_new_score = calc_score[index];
-                best_new_genome = genome_array[index].clone();
             }
         }
+
+        let (highest_new_score, best_new_genome) =
+            find_highest_score_genome(&measured_scores, &genome_array);
 
         if highest_new_score > high_score {
             high_score = highest_new_score;
             output(&best_new_genome);
-        } 
+        }
         if highest_new_score > run_sim_instance.score {
-            not_yet_complete.push(SimulationInstance{genome: best_new_genome, score: highest_new_score});
-        } else if highest_new_score == run_sim_instance.score && complete.len() < COMPLETE_VECTOR_MAX_SIZE {
-            complete.push(SimulationInstance{genome: best_new_genome, score: highest_new_score});
-        } else if highest_new_score == run_sim_instance.score && complete.len() == COMPLETE_VECTOR_MAX_SIZE {
+            not_yet_complete.push(SimulationInstance {
+                genome: best_new_genome,
+                score: highest_new_score,
+            });
+        } else if highest_new_score == run_sim_instance.score
+            && complete.len() < COMPLETE_VECTOR_MAX_SIZE
+        {
+            complete.push(SimulationInstance {
+                genome: best_new_genome,
+                score: highest_new_score,
+            });
+        } else if highest_new_score == run_sim_instance.score
+            && complete.len() == COMPLETE_VECTOR_MAX_SIZE
+        {
             for index in 0..complete.len() {
                 if highest_new_score > complete[index].score {
-                    complete[index] = SimulationInstance{genome: best_new_genome, score: highest_new_score};
+                    complete[index] = SimulationInstance {
+                        genome: best_new_genome,
+                        score: highest_new_score,
+                    };
                     break;
                 }
-
             }
-        } 
-
-
+        }
     }
 }
 
-fn get_sim_instance_to_run<GenomeType>(new_genome: fn() -> GenomeType, 
-    complete: &mut Vec<SimulationInstance<GenomeType>>,
-    not_yet_complete: &mut Vec<SimulationInstance<GenomeType>>) 
-    -> SimulationInstance<GenomeType> {
+fn find_highest_score_genome<GenomeType: Clone>(
+    measured_scores: &[f64; 8],
+    genome_array: &[GenomeType; 8],
+) -> (f64, GenomeType) {
+    let mut highest_new_score = measured_scores[0];
+    let mut best_new_genome_index = 0;
+    for index in 1..measured_scores.len() {
+        if measured_scores[index] > highest_new_score {
+            highest_new_score = measured_scores[index];
+            best_new_genome_index = index;
+        }
+    }
 
+    (
+        highest_new_score,
+        genome_array[best_new_genome_index].clone(),
+    )
+}
+
+fn get_sim_instance_to_run<GenomeType>(
+    new_genome: fn() -> GenomeType,
+    complete: &mut Vec<SimulationInstance<GenomeType>>,
+    not_yet_complete: &mut Vec<SimulationInstance<GenomeType>>,
+) -> SimulationInstance<GenomeType> {
     let mut rng = rand::rng();
 
-    if rng.random_ratio(complete.len().try_into().unwrap(), (3*COMPLETE_VECTOR_MAX_SIZE).try_into().unwrap()) {
+    if rng.random_ratio(complete.len() as u32, (3 * COMPLETE_VECTOR_MAX_SIZE) as u32) {
         let complete_element = complete.pop();
 
         if complete_element.is_some() {
@@ -99,17 +127,21 @@ fn get_sim_instance_to_run<GenomeType>(new_genome: fn() -> GenomeType,
 
     match not_yet_complete.pop() {
         Some(instance) => instance,
-        None => SimulationInstance{genome: new_genome(), score: 0.0},
+        None => SimulationInstance {
+            genome: new_genome(),
+            score: 0.0,
+        },
     }
 }
 
-fn get_mutated_genome<GenomeType>(original_genome: &GenomeType, 
-                                mutators: &Vec<fn(genome: &GenomeType) -> GenomeType>) -> GenomeType {
+fn get_mutated_genome<GenomeType>(
+    original_genome: &GenomeType,
+    mutators: &Vec<fn(genome: &GenomeType) -> GenomeType>,
+) -> GenomeType {
     let mut rng = rand::rng();
 
     mutators[rng.random_range(0..mutators.len())](original_genome)
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -117,7 +149,13 @@ mod tests {
 
     #[test]
     fn test_run_framework() {
-        run_simulation(new_test_genome, calculate_score, output_blocks, vec![mutate], finish);
+        run_simulation(
+            new_test_genome,
+            calculate_score,
+            output_blocks,
+            vec![mutate],
+            finish,
+        );
     }
 
     fn new_test_genome() -> [bool; 25] {
@@ -139,24 +177,24 @@ mod tests {
         let mut score_rtn = 0.0;
 
         if genome[0] && !genome[1] {
-            score_rtn +=1.0;
+            score_rtn += 1.0;
         }
 
-        if !genome[genome.len()-2] && genome[genome.len()-1] {
-            score_rtn +=1.0;
+        if !genome[genome.len() - 2] && genome[genome.len() - 1] {
+            score_rtn += 1.0;
         }
 
-        for i in 1..genome.len()-1 {
-            if !genome[i-1] && genome[i] && !genome[i+1] {
-                score_rtn+=1.0;
-            } 
+        for i in 1..genome.len() - 1 {
+            if !genome[i - 1] && genome[i] && !genome[i + 1] {
+                score_rtn += 1.0;
+            }
         }
 
         score_rtn
     }
 
     fn finish(score: f64) -> bool {
-        score >=13.0
+        score >= 13.0
     }
 
     fn mutate(old_genome: &[bool; 25]) -> [bool; 25] {
@@ -170,5 +208,4 @@ mod tests {
 
         new_genome
     }
-
 }
